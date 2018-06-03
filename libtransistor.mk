a6a3f6acce1bb42f9cb24d1d0483e3ea9c5c4a57
misson20000 <xenotoad@xenotoad.net>
2018-06-02 19:37:04 -0700

    libcxx includes need to come before libc includes

--- libtransistor.mk
+++ libtransistor.mk
@@ -55,7 +55,7 @@ LD_SHARED_LIBRARY_FLAGS := --shared \
 	-Bdynamic
 
 CC_FLAGS := -g -fPIC -fexceptions -fuse-ld=lld -fstack-protector-strong -O3 -mtune=cortex-a53 -target aarch64-none-linux-gnu -nostdlib -nostdlibinc $(SYS_INCLUDES) -D__SWITCH__=1 -Wno-unused-command-line-argument
-CXX_FLAGS := $(CC_FLAGS) -std=c++17 -stdlib=libc++ -nodefaultlibs -nostdinc++ $(CPP_INCLUDES)
+CXX_FLAGS := $(CPP_INCLUDES) $(CC_FLAGS) -std=c++17 -stdlib=libc++ -nodefaultlibs -nostdinc++
 AR_FLAGS := rcs
 AS_FLAGS := -arch=aarch64 -triple aarch64-none-switch
 
